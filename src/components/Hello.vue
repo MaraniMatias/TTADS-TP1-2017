7cf62ef0b173393bdbd5cdbcd2541189e9d18a18
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-08-09 19:30:31 -0300

    base with exameple

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    h1 {{ hello }}
+    p :D
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      hello: 'Hello World! :D'
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,42 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { store } from './store/store';
+/*
+ * VueJS y plugin generales.
+ */
+import './index.less';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+Vue.use(Vuex);
+Vue.use(VueRouter);
+import store from './store';
+/*
+ * Componetes
+ */
+import AppVuex from './components/vuex.vue';
+import Hello from './components/Hello.vue';
+/*
+ * Jugar con las URL desde el navegador.
+ */
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    components: {
+      default: Hello
+    }
+  }, {
+    path: '/vuex',
+    components: {
+      default: AppVuex
+    }
+  }]
+});
+/*
+ * Construye la app apartir del elemento con id root.
+ */
+export default new Vue({
+  el: '#root',
+  router,
+  store: new Vuex.Store(store),
+  render: h => h('router-view')
+});
 
-new Vue({
-  el: '#app',
-  store: store,
-  render: h => h(App)
-})
