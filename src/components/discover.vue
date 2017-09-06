e46ae7ac556e57663373b8c28beeab4bc2970f7e
andres1790 <andresdlg21@gmail.com>
2017-09-06 07:32:28 -0300

    agregado control paginador

--- src/components/discover.vue
+++ src/components/discover.vue
@@ -1,36 +1,32 @@
 <template lang="pug">
-#discover.ui.container
+#discover.column(is-half)
   h1 Movie Discover
-  //.ui.comments
-    .ui.centered.inline.loader(:class="{ active: !discover.total_pages}")
-  .ui.link.cards.three
-    movie-card(v-for="(movie, index) in discover.results" :key="movie.id" :star="movie.vote_average" :title="movie.title" :poster="movie.poster_path" :overview="movie.overview" :release-date="movie.release_date" :genre-ids="movie.genre_ids" :movie-id="movie.id")
+  .ui.link.cards
+    movie-card(v-for="(movie, index) in discover.results" :key="movie.id" :star="movie.vote_average" :title="movie.title" :poster="movie.poster_path" :overview="movie.overview" :release-date="movie.release_date" :genre-ids="movie.genre_ids")
+  paginator(:pages="discover.total_pages" :page="discover.page")
 </template>
 
 <script>
 import { mapGetters, mapActions, mapState } from 'vuex'
 import movieCard from './movieCard.vue';
+import paginator from './paginator.vue'
 
 export default {
   name: 'discover',
-  data() {
+  data(){
     return {
-      page: 1
+      page: 2
     }
   },
   components: {
-    movieCard
+    movieCard,
+    paginator
   },
-  computed: {
-    ...mapState([
+  computed: mapState([
     'discover'
   ]),
-    loadMovie: function () {
-      this.$store.dispatch('loadMovieDiscover', this.page)
-    }
-  },
   mounted: function () {
-    this.loadMovie;
+    this.$store.dispatch('loadMovieDiscover',this.page)
   }
 }
 </script>
---
+++ src/components/paginator.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="ui centered grid">
+  <div class="center aligned column">
+    <div class="ui pagination menu">
+      <a class="item">
+      <i class="angle left icon"></i>
+    </a>
+    <!--<a v-for:"page in pages">
+      <a class="item">
+      {{indexOf(page)}}
+    </a>
+    </a>-->
+    <a class="active item">
+      {{page}}
+    </a>
+      <!--<div class="disabled item">
+        ...
+      </div>-->
+    <a class="item">
+      {{page + 1}}
+    </a>
+      <a class="item">
+      {{page + 2}}
+    </a>
+    <a class="item">
+      {{page + 3}}
+    </a>
+    <a class="item">
+      {{page + 4}}
+    </a>
+      <a class="item">
+      <i class="angle right icon"></i>
+    </a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'paginator',
+  props: ['pages','page'],
+  data(){
+    return{
+      limit: 5
+    }
+  }
+}
+</script>
