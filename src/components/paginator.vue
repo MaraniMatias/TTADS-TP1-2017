06161ee8b3c26552ff7fd17d499ce5616ac06452
andres1790 <andresdlg21@gmail.com>
2017-09-06 07:45:50 -0300

    primer version funcional

--- src/components/paginator.vue
+++ src/components/paginator.vue
@@ -2,35 +2,27 @@
 <div class="ui centered grid">
   <div class="center aligned column">
     <div class="ui pagination menu">
-      <a class="item">
-      <i class="angle left icon"></i>
-    </a>
-    <!--<a v-for:"page in pages">
-      <a class="item">
-      {{indexOf(page)}}
-    </a>
-    </a>-->
-    <a class="active item">
-      {{page}}
-    </a>
-      <!--<div class="disabled item">
-        ...
-      </div>-->
-    <a class="item">
-      {{page + 1}}
-    </a>
-      <a class="item">
-      {{page + 2}}
-    </a>
-    <a class="item">
-      {{page + 3}}
-    </a>
-    <a class="item">
-      {{page + 4}}
-    </a>
-      <a class="item">
-      <i class="angle right icon"></i>
-    </a>
+      <a class="item" @click="updateDiscover(page-1)">
+        <i class="angle left icon"></i>
+      </a>
+      <a class="active item">
+        {{page}}
+      </a>
+      <a class="item" @click="updateDiscover(page+1)">
+        {{page + 1}}
+      </a>
+        <a class="item" @click="updateDiscover(page+2)">
+        {{page + 2}}
+      </a>
+      <a class="item" @click="updateDiscover(page+3)">
+        {{page + 3}}
+      </a>
+      <a class="item" @click="updateDiscover(page+4)">
+        {{page + 4}}
+      </a>
+      <a class="item" @click="updateDiscover(page+5)">
+        <i class="angle right icon"></i>
+      </a>
     </div>
   </div>
 </div>
@@ -43,6 +35,14 @@ export default {
   data(){
     return{
       limit: 5
+    }
+  },
+  methods:{
+    updateDiscover: function(pag){
+      console.log(pag);
+      if(pag != 0){
+        this.$store.dispatch('loadMovieDiscover',pag)
+      }
     }
   }
 }
