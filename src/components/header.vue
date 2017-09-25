0b9e22f4423cc8e5745763af99a22ea178c49994
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-09-24 23:15:01 -0300

    fix css menu and sear-bar

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,15 +1,47 @@
 <template>
-  <div class="ui inverted menu">
+<div>
+  <div class="ui inverted menu descktop">
     <router-link class="item teal" :to="{ name: 'discover' }" exact>Home</router-link>
     <router-link class="item teal" :to="{ name: 'docs' }" exact>Docs</router-link>
     <a class="item teal" href="https://github.com/MaraniMatias/tp-2017">Repo Git</a>
-    <searchBar style="width:80%; margin-top:5px"></searchBar>
+    <div class="item search-bar">
+      <search-bar></search-bar>
+    </div>
     <div class="right menu">
-      <div class="item">
-        <button class="ui active button"><i class="user icon"></i>Iniciar Sesión</button>
+      <div class="ui item">
+        <button class="ui button"><i class="user icon"></i>Iniciar Sesión</button>
       </div>
     </div>
   </div>
+  <div class="ui icon inverted menu movil">
+    <router-link class="item teal" :to="{ name: 'discover' }" exact>
+      <i class="home icon"></i>
+    </router-link>
+    <router-link class="item teal" :to="{ name: 'docs' }" exact>
+      <i class="book icon"></i>
+    </router-link>
+    <a class="item teal" href="https://github.com/MaraniMatias/tp-2017">
+    <i class="github icon"></i>
+  </a>
+    <!--
+    <vid class="item teal link">
+      <i class="search icon"></i>
+    </vid>
+    -->
+    <div class="right menu">
+      <div class="ui item link">
+        <i class="user icon"></i>
+      </div>
+    </div>
+  </div>
+  <div class="ui container">
+    <div class="ui grid">
+      <div class="wide column">
+        <search-bar></search-bar>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -29,5 +61,25 @@ export default {
 .ui.inverted.menu .router-link-active {
   background-color: #00b5ad;
   border-radius: 0px;
+}
+
+.item.search-bar {
+  margin: auto;
+  flex: auto;
+}
+
+@media only screen and (max-device-width: 660px) {
+  .ui.inverted.menu.descktop {
+    display: none;
+  }
+}
+
+@media only screen and (min-device-width: 661px) {
+  .movil {
+    display: none;
+  }
+  .ui.container {
+    display: none;
+  }
 }
 </style>
