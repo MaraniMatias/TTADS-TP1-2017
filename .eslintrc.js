74a309fbfcc866fdc691ede45e56782306ee87fa
Marani Matias Ezequiel <maranimatias@gmail.com>
2017-09-26 19:50:31 -0300

    add up globals on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,17 @@ module.exports = {
   //"airbnb-base",
     'eslint:recommended',
     'plugin:vue/recommended'
-  ]
+  ],
+  env: {
+    browser: true,
+    node: true,
+    mocha: true,
+    jasmine: true,
+    phantomjs: true
+  },
+  globals: {
+    axios: true,
+    state: true,
+    commit: true
+  }
 };
